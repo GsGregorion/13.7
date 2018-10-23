adefb4b722fa0efec3771d703a5563a8ca46ef70
Gregorion Skquarna <gs.gregorion@gmail.com>
2018-10-23 12:21:34 +0200

    Finish kodilla task

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-var OSinfo = require('./modules/OSinfo');
+var readDirMkLogFile = require('./modules/readDir.js');
 
-process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-        switch (instruction) {
-            case '/exit':
-            case 'quit':
-            case 'q':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case 'ver':
-                console.log(process.env);
-                break;
-            case 'Papayas':
-                process.stdout.write('Mangoes and papayas are very tasty :) This is my case test \n');
-                break;
-            case '/getOSinfo':
-            case 'i':
-                OSinfo.print();
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\nCreate something different! \n');
-                break;
-        }
-    }
-});
+readDirMkLogFile.print();
